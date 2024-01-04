9df30376daf33fc61b4607126c92200bd9706ef3
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-04 22:13:38 +0700

    setup env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') // buat setup .env bisa kebaca vite
+  return {
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      setupFiles: ['./tests/setup.js'],
+      testMatch: ['./tests/**/*.test.jsx'],
+      globals: true
+    },
+    define: {
+      'process.env.BASE_API': JSON.stringify(env.BASE_API),
+      // case jika tidak ada boolean value di env
+      // 'process.env': JSON.stringify(env)
+    }
+  }
 })
