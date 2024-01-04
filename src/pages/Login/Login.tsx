2547bac41779ad7bd30dbde3b3d356312101cf3d
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-05 00:06:52 +0700

    setup font

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,17 +3,19 @@ import './login.css'
 import { iconFoodbank2 } from '../../image'
 function Login() {
 
-    // const [formData, setFormData] = useState({
-    //     email: "",
-    //     password: "",
-    // })
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
 
     return (
         <section className="login" id='login'>
             <div className="container d-flex justify-content-center align-items-center">
-                <div className="card_login">
-                    <img src={iconFoodbank2} alt="" />
-                    <p className='text-black' >bajing</p>
+                <div className="card_login ">
+                    <div className="d-flex justify-content-center mb-2">
+                        <img src={iconFoodbank2} alt="icons-login" />
+                    </div>
+                    <h1 className='text-center'>Food Bank</h1>
                 </div>
             </div>
         </section>
