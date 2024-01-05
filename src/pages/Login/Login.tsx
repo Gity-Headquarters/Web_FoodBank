bbf8d5251d91435d208d9bea8cbf2b1e5a5fec8e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-05 14:41:31 +0700

    disable error message

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,8 +4,6 @@ import { iconFoodbank2 } from '../../image'
 import InputForm from '../../components/Elements/inputForm/InputForm'
 // import { login } from '../../service/Authentication'
 function Login() {
-
-    const [errorMsg, setErrorMsg] = useState("d-none")
     const [buttonLogin, setButtonLogin] = useState(false)
     const [disabled, setDisabled] = useState(true)
 
@@ -67,7 +65,7 @@ function Login() {
                     <form>
                         <InputForm htmlFor="email" onChange={handleChange} type="email" value={formData.email} placeholder="Masukkan Email" />
                         <InputForm htmlFor="password" onChange={handleChange} type="password" value={formData.password} placeholder="Masukkan Password" />
-                        <p className={` text danger ${errorMsg}`} > Incorect Email or Password</p>
+                        {/* <p className={` text danger`} > Incorect Email or Password</p> */}
                         <button disabled={disabled} className={`btn ${buttonLogin ? 'btn-login' : 'btn-secondary text-white-50'} w-100 fw-semibold fs-5 mt-5`} >Masuk</button>
                     </form>
                 </div>
