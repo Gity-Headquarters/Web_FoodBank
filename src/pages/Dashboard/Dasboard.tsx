4a81a6357d832be85ff84d5c503a445b755c14ff
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-08 07:47:15 +0700

    starting slicing

--- src/pages/Dashboard/Dasboard.tsx
+++ src/pages/Dashboard/Dasboard.tsx
@@ -1,6 +1,5 @@
 import Layout from "../../Layout/Layout"
 import TitlePage from "../../components/Elements/TitlePage/TitlePage"
-import CardLink from "../../components/Fragments/CardLink/CardLink"
 
 
 function Dasboard() {
@@ -8,9 +7,6 @@ function Dasboard() {
         <Layout>
             <section className="dashboard " >
                 <TitlePage title="Dashboard" />
-                <CardLink location="/" >
-                    <p>tolong klik !</p>
-                </CardLink>
             </section>
         </Layout>
     )
