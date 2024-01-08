232a5019d80359cb958470922e18161dbc59d8aa
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-08 15:12:54 +0700

    create dana admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Dasboard from './pages/Dashboard/Dasboard';
+import DanaAdmin from './pages/DanaAdmin/DanaAdmin';
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dasboard />} />
+      <Route path="/dana-admin" element={<DanaAdmin />} />
     </Routes>
 
   )
---
+++ src/pages/DanaAdmin/DanaAdmin.tsx
@@ -0,0 +1,14 @@
+import Layout from "../../Layout/Layout"
+import TitlePage from "../../components/Elements/TitlePage/TitlePage"
+
+function DanaAdmin() {
+    return (
+        <Layout>
+            <section className="dana-admin" id="dana-admin">
+                <TitlePage title="Dana Admin" />
+            </section>
+        </Layout>
+    )
+}
+
+export default DanaAdmin
