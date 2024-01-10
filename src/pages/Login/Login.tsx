9980cc36e2d2b153e9c49d78d9a8d02f96621d7c
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-01-10 16:51:01 +0700

    aman

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './login.css'
 import { logoLogin } from '../../image'
-import InputForm from '../../components/Elements/inputForm/InputForm'
+import InputForm from '../../components/Elements/Input/Input'
 // why its error
 import { login } from '../../service/Authentication'
 // import { login } from '../../service/Authentication'
